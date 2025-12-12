33ec548bae6a88d60954c56b128efdbb1fa3ef34
Danilo Cucharro <danilocucharro@Danilos-MacBook-Air.local>
2025-12-12 15:13:10 -0300

    feat: error handler for when the user try to make a request with the wrong body schema

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
-import Fastify from "fastify";
+import Fastify, { type FastifyReply } from "fastify";
 import { organizationsRoutes } from "./http/controllers/organizations/routes.js";
+import z, { ZodError } from "zod";
 
 export const app = Fastify();
 
 organizationsRoutes(app);
+
+app.setErrorHandler((error: Error, _, reply: FastifyReply) => {
+  if (error instanceof ZodError) {
+    reply.status(500).send({
+      message: z.treeifyError(error),
+    });
+  }
+  reply.status(500).send({
+    message: error.message,
+  });
+});
